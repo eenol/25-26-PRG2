4ff7991c790ab637fb34a334be5c049506ed2366
eenol <enol.hidalgo@alumnos.uneatlantico.es>
2026-02-10 13:59:42 +0100

    feat: impresion de lineas en pantalla

--- entregas/hidalgoEnol/PyEdlin.java
+++ entregas/hidalgoEnol/PyEdlin.java
@@ -8,3 +8,11 @@ String[] lineas = new String[10];
 for (int i = 0; i < lineas.length; i++) {
     lineas[i] = "";
 }
+public static void mostrarLineas(String[] lineas, int activa) {
+    System.out.println("--------------------------------------------------");
+    for (int i = 0; i < lineas.length; i++) {
+        String marca = (i == activa) ? "*" : " ";
+        System.out.println(i + ":" + marca + "| " + lineas[i]);
+    }
+    System.out.println("--------------------------------------------------");
+}
