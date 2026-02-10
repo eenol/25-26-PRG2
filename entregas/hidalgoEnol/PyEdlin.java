c463ac9542eb4129faf62d18f629fe55f5edb84f
eenol <enol.hidalgo@alumnos.uneatlantico.es>
2026-02-10 14:05:07 +0100

    error, se vuelve a empezar

--- entregas/hidalgoEnol/PyEdlin.java
+++ entregas/hidalgoEnol/PyEdlin.java
@@ -3,16 +3,3 @@ public class PyEdlin {
         System.out.println("Bienvenido a pyEdlin");
     }
 }
-String[] lineas = new String[10];
-
-for (int i = 0; i < lineas.length; i++) {
-    lineas[i] = "";
-}
-public static void mostrarLineas(String[] lineas, int activa) {
-    System.out.println("--------------------------------------------------");
-    for (int i = 0; i < lineas.length; i++) {
-        String marca = (i == activa) ? "*" : " ";
-        System.out.println(i + ":" + marca + "| " + lineas[i]);
-    }
-    System.out.println("--------------------------------------------------");
-}
