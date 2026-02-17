fc29ad96cd655bf233562398f46f66e9bb80e004
eenol <enol.hidalgo@alumnos.uneatlantico.es>
2026-02-17 13:51:02 +0100

    Comparar Strings con ==, esto en Java no se puede hacer

--- entregas/hidalgoenol/ExamenMina.java
+++ entregas/hidalgoenol/ExamenMina.java
@@ -22,66 +22,94 @@ public class ExamenMina{
 			{"5", 	"0", 	"0",	"0", 	"0", 	"0", 	"0",	"0"}
 		};
 
-        public static void main(String[] args){	
+    public static void main(String[] args){	
+
 	Scanner scan = new Scanner(System.in);
 		
 		int i=0;	
+
 		while ( i < 5){
+
 			int PosYMina = (int)(Math.random()*2+1);
 			int PosXMina = (int)(Math.random()*4+1);
-			if (mapaMinasActivas[PosXMina][PosYMina]=="1"){
-			i=i-1;
+
+			// CAMBIO AQUÍ
+			if (mapaMinasActivas[PosXMina][PosYMina].equals("1")){
+
+				i=i-1;
 			}
 			else {
+
 				mapaMinasActivas[PosXMina+1][PosYMina+1]="1";
 			}
+
 			i++;
 		}
 	
 		int posX=0, posY=0, juegoEncendido=1, valoresCorrectos, contadorMapa=0, contadorMinas=0;
 
         while(juegoEncendido==1){
+
 			for( i = 0; i<mapaMinasMostrar.length; i++){            
+
 				for (int j = 0; j<=mapaMinasMostrar.length+1; j++) {                                     
+
 					System.out.print(mapaMinasMostrar[i][j]);
 				}
+
 				System.out.println(" ");
 			}
-			valoresCorrectos=0;//reseteo de la variable
+
+			valoresCorrectos=0;
+
 			while(valoresCorrectos==0){
+
 				System.out.println(" ");
 				System.out.println("Ingrese X");
 				posY = scan.nextInt();
+
 				System.out.println("Ingrese Y");
 				posX = scan.nextInt();
+
 				if(posX>5){
+
 					valoresCorrectos=0;
 				}
 				else if(posY>7){
+
 					valoresCorrectos=0;
 				}
 				else{
+
 					valoresCorrectos=1;
 				}
 			}
 
-			if(mapaMinasActivas[posX][posY]=="1"){
-			mapaMinasMostrar[posX][posY]="x";
-			contadorMinas++;
+			if(mapaMinasActivas[posX][posY].equals("1")){
+
+				mapaMinasMostrar[posX][posY]="x";
+				contadorMinas++;
 			}
-			else if(mapaMinasActivas[posX][posY]=="0"){
-			mapaMinasMostrar[posX][posY]=".";
+
+			else if(mapaMinasActivas[posX][posY].equals("0")){
+
+				mapaMinasMostrar[posX][posY]=".";
 			}
+
 			else{
-			System.out.println("No es correcta esa opcion.");
+
+				System.out.println("No es correcta esa opcion.");
 			}
 			
 			contadorMapa++;
+
 			if(contadorMinas>2){
+
 				juegoEncendido=0;
 				System.out.println("Has perdido");
 			}
 			else if(contadorMapa>=31){
+
 				System.out.println("Felicidades Ganador!");
 				juegoEncendido=0;
 			}
@@ -90,6 +118,4 @@ public class ExamenMina{
 			}
 		}
 	}	
-		
-		
 }
